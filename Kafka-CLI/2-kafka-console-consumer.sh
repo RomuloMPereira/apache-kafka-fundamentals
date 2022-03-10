43fc7adaf7f67a0f765876807ea370069f857be2
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-03-10 18:23:27 -0300

    kafka-console-consumer

---
+++ Kafka-CLI/2-kafka-console-consumer.sh
@@ -0,0 +1,7 @@
+kafka-console-consumer 
+
+# consuming
+kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic first_topic
+
+# consuming from beginning
+kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic first_topic --from-beginning
