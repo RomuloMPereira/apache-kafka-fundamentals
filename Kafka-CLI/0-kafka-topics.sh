b83570fea977c49cda2c075866b3c1749de1539e
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-03-11 14:13:02 -0300

    Start zookeper and kafka

--- Kafka-CLI/0-kafka-topics.sh
+++ Kafka-CLI/0-kafka-topics.sh
@@ -1,13 +1,18 @@
-# Replace "kafka-topics" 
-# by "kafka-topics.sh" or "kafka-topics.bat" based on your system 
-# (or bin/kafka-topics.sh or bin\windows\kafka-topics.bat if you didn't setup PATH / Environment variables)
+# Start Zookeper
+zookeeper-server-start.bat config\zookeeper.properties
 
+# Start Kafka
+kafka-server-start.bat config\server.properties
+
+# Create, delete, describe, or change a topic
 kafka-topics 
 
+# Create topic
 # Replication factor is equal to 1 because there is only one broker available for now
-
 kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create --partitions 3 --replication-factor 1
 
+# List topics
 kafka-topics --zookeeper 127.0.0.1:2181 --list 
 
+#Describe a topic
 kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --describe
